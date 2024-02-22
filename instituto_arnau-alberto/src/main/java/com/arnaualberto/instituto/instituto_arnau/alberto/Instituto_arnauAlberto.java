84fdc0bed4700c5042c8b9806f0a7b4a4581b789
Arnau <arnausolereig@gmail.com>
2024-02-22 13:21:34 +0100

    primer commit

---
+++ instituto_arnau-alberto/src/main/java/com/arnaualberto/instituto/instituto_arnau/alberto/Instituto_arnauAlberto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.arnaualberto.instituto.instituto_arnau.alberto;
+
+/**
+ *
+ * @author Mati
+ */
+public class Instituto_arnauAlberto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
