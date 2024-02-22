3076193bbf99cae2859d6af50d2a97458ba013d4
Arnau <arnausolereig@gmail.com>
2024-02-22 13:30:11 +0100

    segundo commit

--- instituto_arnau-alberto/src/main/java/com/arnaualberto/instituto/instituto_arnau/alberto/Instituto_arnauAlberto.java
+++ instituto_arnau-alberto/src/main/java/com/arnaualberto/instituto/instituto_arnau/alberto/Instituto_arnauAlberto.java
@@ -11,6 +11,6 @@ package com.arnaualberto.instituto.instituto_arnau.alberto;
 public class Instituto_arnauAlberto {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Worooold!");
     }
 }
